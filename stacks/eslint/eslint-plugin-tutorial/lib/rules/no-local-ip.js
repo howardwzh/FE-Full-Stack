9285e990d1dade22e08a6b75bfcea1c2cbaf701d
王忠华 <wangzhonghua@ppmoney.com>
2018-06-13 10:19:09 +0800

    update: eslint plugin

--- stacks/eslint/eslint-plugin-tutorial/lib/rules/no-local-ip.js
+++ stacks/eslint/eslint-plugin-tutorial/lib/rules/no-local-ip.js
@@ -1,11 +1,13 @@
+"use strict";
+
 var onLocalIP = require('../utils/check-list').onLocalIP;
 
 module.exports = {
   meta: {
     docs: {
-      description: "线上发布脚本中不能含有本地测试ip！请删除后再执行发布操作。",
-      category: "Possible Errors",
-      recommended: true
+      description: '线上发布脚本中不能含有本地测试ip！请删除后再执行发布操作。',
+      category: 'Possible Errors',
+      recommended: false
     },
     schema: []
   },
@@ -13,7 +15,7 @@ module.exports = {
     return {
       Literal: function (node) {
         if (onLocalIP.rule.test(node.value)) {
-          context.report(node, onLocalIP.msg);
+          context.report({node: node, message: onLocalIP.msg});
         }
       }
     };
